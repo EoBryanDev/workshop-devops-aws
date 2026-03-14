9ef9d6569830ba5425bda58d7d28da2d9926f2f9
zOS-Bryan-Galaxy <eobryandev@gmail.com>
2026-03-14 13:19:43 -0300

    feat: creating remote-bknd on bucket tfstate management

--- aws/00-remote-backend/main.tf
+++ aws/00-remote-backend/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "workshop-march-remote-bknd-bucket-bryan"
+    key    = "bootstrap/terraform.tfstate" 
+    region = "us-east-2"
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
